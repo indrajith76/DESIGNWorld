4dd7d3ccc12ad3b8b448cf30ada590b6f29eed90
indrajith76 <indrajithgoswami@gmail.com>
2024-06-05 22:34:52 +0600

    added features

---
+++ src/components/AddServiceModal.jsx
@@ -0,0 +1,129 @@
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+
+const AddServiceModal = ({ trigger, setTrigger }) => {
+  const [formData, setFormData] = useState({
+    icon: "",
+    serviceName: "",
+    description: "",
+    price: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:3000/services", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        toast.success("Added Service Successfully!");
+        setTrigger(!trigger);
+        document.getElementById("my_modal_3").close();
+      });
+  };
+
+  return (
+    <dialog id="my_modal_3" className="modal">
+      <div className="modal-box">
+        <form method="dialog">
+          {/* if there is a button in form, it will close the modal */}
+          <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+            ✕
+          </button>
+        </form>
+        <h3 className="font-bold text-lg text-center">Add Service</h3>
+
+        <form
+          onSubmit={handleSubmit}
+          className="max-w-lg mx-auto p-8 bg-white rounded-lg"
+        >
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="icon"
+            >
+              Service Icon
+            </label>
+            <input
+              type="text"
+              id="icon"
+              name="icon"
+              value={formData.icon}
+              onChange={handleChange}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="serviceName"
+            >
+              Service Name
+            </label>
+            <input
+              type="text"
+              id="serviceName"
+              name="serviceName"
+              value={formData.serviceName}
+              onChange={handleChange}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="description"
+            >
+              Description
+            </label>
+            <textarea
+              id="description"
+              name="description"
+              value={formData.description}
+              onChange={handleChange}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            ></textarea>
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="price"
+            >
+              Price
+            </label>
+            <input
+              type="text"
+              id="price"
+              name="price"
+              value={formData.price}
+              onChange={handleChange}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              type="submit"
+              className="bg-orange-600 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </dialog>
+  );
+};
+
+export default AddServiceModal;
--- src/pages/AllServices/AllServices.jsx
+++ src/pages/AllServices/AllServices.jsx
@@ -2,21 +2,38 @@ import { FaRegEdit } from "react-icons/fa";
 import { RiDeleteBin5Fill } from "react-icons/ri";
 import { useState } from "react";
 import { useEffect } from "react";
+import AddServiceModal from "../../components/AddServiceModal";
+import toast from "react-hot-toast";
 
 const AllServices = () => {
   const [services, setServices] = useState([]);
+  const [trigger, setTrigger] = useState(true);
 
   useEffect(() => {
     fetch("https://design-world-server.vercel.app/services")
       .then((res) => res.json())
       .then((data) => setServices(data));
-  }, []);
+  }, [trigger]);
+
+  const deleteHandler = (id) => {
+    fetch(`http://localhost:3000/services/${id}`, {
+      method: "delete",
+    })
+      .then((res) => res.json())
+      .then(() => {
+        setServices(!trigger);
+        toast.success("Deleted Successfully!");
+      });
+  };
 
   return (
     <div>
       <div className="mb-10 flex items-center justify-between">
         <h1 className="text-2xl">AllServices</h1>
-        <button className="btn btn-sm bg-sky-700 text-white">
+        <button
+          onClick={() => document.getElementById("my_modal_3").showModal()}
+          className="btn btn-sm bg-sky-700 text-white"
+        >
           Add Service
         </button>
       </div>
@@ -39,16 +56,19 @@ const AllServices = () => {
               <tr key={idx}>
                 <th>{idx + 1}</th>
                 <td className="flex justify-center">
-                  <i className={`${service.icon} fa-3x`}></i>
+                  <i className={`${service?.icon} fa-3x`}></i>
                 </td>
-                <td>{service.title}</td>
-                <td>{service.description}</td>
-                <td className="text-lg">${service.price}</td>
+                <td>{service?.title}</td>
+                <td>{service?.description}</td>
+                <td className="text-lg">${service?.price}</td>
                 <td className="flex items-center justify-center gap-2">
                   <button className="btn btn-sm btn-success text-white">
                     <FaRegEdit />
                   </button>
-                  <button className="btn btn-sm btn-error text-white">
+                  <button
+                    onClick={() => deleteHandler(service?._id)}
+                    className="btn btn-sm btn-error text-white"
+                  >
                     <RiDeleteBin5Fill />
                   </button>
                 </td>
@@ -57,6 +77,7 @@ const AllServices = () => {
           </tbody>
         </table>
       </div>
+      <AddServiceModal trigger={trigger} setTrigger={setTrigger} />
     </div>
   );
 };
